b62053df367eecfbca35df441ac480dc24538420
mberg91 <142218756+mberg91@users.noreply.github.com>
2023-08-18 15:01:51 +0300

    Update streamlit_app.py
    Updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,4 @@ streamlit.dataframe(my_data_rows)
 
 #second text entry box
 fruit_choice = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding', fruit_choice)
