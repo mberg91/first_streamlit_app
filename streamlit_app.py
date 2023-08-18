fac72b16ad510d04675bf4d6649cd84bb8906253
mberg91 <142218756+mberg91@users.noreply.github.com>
2023-08-18 15:32:00 +0300

    Update streamlit_app.py
    Updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+#don't run anything past here while we troubleshoot
+streamlit.stop()
+
 #Let's query our trial account metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -42,9 +45,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
-#don't run anything past here while we troubleshoot
-streamlit.stop()
 
 #second text entry box
 fruit_choose = streamlit.text_input('What fruit would you like to add?')
